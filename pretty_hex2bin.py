50293b59aba15c085e0a6d1071c47be4e2c13fba
Manfred Touron <github@42.am>
2012-07-06 15:46:42 +0200

    added history support + some new colours

--- pretty_hex2bin.py
+++ pretty_hex2bin.py
@@ -2,6 +2,7 @@
 
 import re
 import readline
+import atexit
 
 def color_binary(binary):
     return binary.replace('0', '*TMP*').replace('1', '\033[1;32m1\033[0m').replace('*TMP*', '\033[0;33m0\033[0m')
@@ -20,22 +21,29 @@ def pretty_hex2bin(input):
     binary = "{0:020b}".format(hex).zfill(32)
     pretty_binary = group_binary(binary)
     colored_pretty_binary = color_binary(pretty_binary)
-    out = "%14s     ->     %s\n" % (number, colored_pretty_binary)
+    out = "\033[1;31m%14s\033[0m    ->      %s\n" % (number, colored_pretty_binary)
     for bitset in bitsets:
         if ':' in bitset:
             bitset_numbers = map(int, bitset.split(':'))
         else:
             bitset_numbers = [int(bitset), int(bitset)]
-        print bitset_numbers
         bitset_binary = binary[32 - max(bitset_numbers) - 1:32 - min(bitset_numbers)]
         padded_bitset_binary = '.' * (32 - max(bitset_numbers) - 1) + bitset_binary + '.' * (min(bitset_numbers))
         pretty_padded_bitset_binary = group_binary(padded_bitset_binary)
         colored_bitset_binary = color_binary(pretty_padded_bitset_binary)
         bitset_value = int(bitset_binary, 2)
-        out += '       %-7s      %-2s    %s\n' % ('[%s]' % bitset, bitset_value, colored_bitset_binary)
+        out += '       \033[1;34m%7s\033[0m    %-5s   %s\n' % ('[%s]' % bitset, bitset_value, colored_bitset_binary)
     return out
 
 if __name__ == "__main__":
+    histfile = './.pb_history'
+    try:
+        readline.read_history_file(histfile)
+    except:
+        pass
+    atexit.register(readline.write_history_file, histfile)
+    del readline, histfile
+
     from sys import argv
     print '                          0000 0000|0011 1111|1111 2222|2222 2233'
     print '                          0123 4567|8901 2345|6789 0123|4567 8901'
