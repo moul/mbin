2c7c94946c5c596bc6b24b28204b8943bd72495b
Manfred Touron <m@42.am>
2012-11-14 16:48:49 +0100

    added support for already processed keys

--- google_authenticator.py
+++ google_authenticator.py
@@ -31,25 +31,33 @@ The same can be achieved with http://www.nongnu.org/oath-toolkit/.
 
 import hmac, base64, struct, hashlib, time, ConfigParser, os.path
 
-def get_hotp_token(secret, intervals_no):
-    key = base64.b32decode(secret, casefold=True)
+def get_hotp_token(key, intervals_no):
     msg = struct.pack(">Q", intervals_no)
     h = hmac.new(key, msg, hashlib.sha1).digest()
     o = ord(h[19]) & 15
     h = (struct.unpack(">I", h[o:o+4])[0] & 0x7fffffff) % 1000000
     return h
 
-def get_totp_token(secret):
-    return get_hotp_token(secret, intervals_no=int(time.time())//30)
+def get_totp_token(key):
+    return get_hotp_token(key, intervals_no=int(time.time())//30)
 
 def main():
     config = ConfigParser.RawConfigParser()
     config.read(os.path.expanduser('~/cu/.gauth'))
     for section in config.sections():
-        secret = config.get(section, 'secret')
-        current = str(get_totp_token(secret)).zfill(6)
-        next = str(get_hotp_token(secret, intervals_no=int(time.time()+30)//30)).zfill(6)
-        print '%s, current=%s, next=%s' % (section, current, next)
+        try:    secret = config.get(section, 'secret')
+        except: secret = None
+        try:    key = config.get(section, 'key')
+        except: key = None
+        try:    hexkey = config.get(section, 'hexkey')
+        except: hexkey = None
+        if hexkey:
+            key = hexkey.decode('hex')
+        if secret:
+            key = base64.b32decode(secret, casefold = True)
+        current = str(get_totp_token(key)).zfill(6)
+        next = str(get_hotp_token(key, intervals_no=int(time.time()+30)//30)).zfill(6)
+        print '%-36s current=%s, next=%s' % ("%s, " % section, current, next)
 
     remaining = int(30 - (time.time() % 30))
     print
