d36f7b6cdea3cf33ef0c35d89608812626deba7e
Manfred Touron <m@42.am>
2013-01-02 17:23:05 +0100

    added stderr redirection to stdout

--- watch.sh
+++ watch.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
 command=$@
-while true; do a=$($command); clear; date;echo; echo $command;echo;echo "$a" ;sleep .5; done
+while true; do
+    a=$($command 2>&1)
+    clear
+    date
+    echo
+    echo $command
+    echo
+    echo "$a"
+    sleep .5
+done
