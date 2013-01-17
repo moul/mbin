083bde8a5857d41c2ef5accc17e1668740112c7b
Manfred Touron <m@42.am>
2013-01-17 11:26:19 +0100

    added git-log-compact.sh

---
+++ git-log-compact.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# from: http://alias.sh/compact-colorized-git-log
+
+git log --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
