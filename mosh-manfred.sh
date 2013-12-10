c4ee1d57ecb46d200b3eb995a5ad075d1c30ddd9
Manfred Touron <m@42.am>
2013-12-11 00:21:49 +0100

    updated mosh helper

--- mosh-manfred.sh
+++ mosh-manfred.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
+RETRY=5
 while true; do
-    mosh $1 -- tmux attach $2 -t manfred
-    echo "reconnecting to $2 in 2 secs"
-    sleep 2
+    mosh $1 -- /bin/sh -c 'tmux attach -t manfred $@ || tmux new-session -s manfred'
+    echo "reconnecting to $1 in 5 secs"
+    sleep 5
 done
-
