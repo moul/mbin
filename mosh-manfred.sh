207343886bc2ad7505a72692c5ecbac6c0d2db58
Manfred Touron <m@42.am>
2013-12-11 12:17:14 +0100

    added /bin/sh fallback

--- mosh-manfred.sh
+++ mosh-manfred.sh
@@ -2,7 +2,7 @@
 
 RETRY=5
 while true; do
-    mosh $1 -- /bin/sh -c 'tmux attach -t manfred $@ || tmux new-session -s manfred'
+    mosh $1 -- /bin/sh -c 'tmux attach -t manfred $@ || tmux new-session -s manfred || /bin/sh'
     echo "reconnecting to $1 in 5 secs"
     sleep 5
 done
--- ssh-manfred.sh
+++ ssh-manfred.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-ssh $1 -t "tmux attach $2 -t manfred || tmux new-session -s manfred"
+ssh $1 -t "tmux attach $2 -t manfred || tmux new-session -s manfred || /bin/sh"
