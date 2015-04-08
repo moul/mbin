e8f14eb08598179957f3b9df2e08b5a8f242edc6
Manfred Touron <m@42.am>
2015-04-08 19:01:53 +0200

    Added max_bg_procs

---
+++ max_bg_procs.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# credits: http://stackoverflow.com/questions/1537956/bash-limit-the-number-of-concurrent-jobs
+
+function max_bg_procs {
+    if [[ $# -eq 0 ]] ; then
+        echo "Usage: max_bg_procs NUM_PROCS.  Will wait until the number of background (&)"
+        echo "           bash processes (as determined by 'jobs -pr') falls below NUM_PROCS"
+        return
+    fi
+    local max_number=$((0 + ${1:-0}))
+    while true; do
+        local current_number=$(jobs -pr | wc -l)
+        if [[ $current_number -lt $max_number ]]; then
+            break
+        fi
+        sleep 1
+    done
+}
