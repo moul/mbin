3706420e387a85c8795439156e6e5a245c61564f
Manfred Touron <m@42.am>
2019-12-18 15:39:12 +0100

    chore: default emacs is current dir

--- emacs.sh
+++ emacs.sh
@@ -3,8 +3,13 @@
 # Checks if there's a frame open
 emacsclient -n -e "(if (> (length (frame-list)) 1) 't)" 2>/dev/null | grep t &>/dev/null
 
+ARGS="$@"
+if [ "$ARGS" = "" ]; then
+    ARGS="."
+fi
+
 if [ "$?" -eq "1" ]; then
-    emacsclient -a '' -nw "$@"
+    emacsclient -a '' -nw "$ARGS"
 else
-    emacsclient -nw "$@"
+    emacsclient -nw "$ARGS"
 fi
